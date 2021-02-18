43c30fa45e376930b13c815a46edf33ec7891f54
Devyn Smith <devyn.smith17@gmail.com>
2021-02-17 17:39:53 -0700

    Add project files.

---
+++ Models/BooksDb.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment5_DevynSmith_Section3.Models
+{
+    public class Book
+    {
+        [Key]
+        [Required]
+        public int BookId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string AuthorFirstName { get; set; }
+        public string? AuthorMiddleName { get; set; }
+        [Required]
+        public string AuthorLastName { get; set; }
+        [Required]
+        public string Publisher { get; set; }
+        [Required]
+        [RegularExpression(@"\d{3}-\d{10}", ErrorMessage = "ISBN incorrect, please use this format: 000-0000000000")]
+        public string Isbn { get; set; }
+        [Required]
+        public string Classification { get; set; }
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        public double Price { get; set; }
+
+    }
+}
---
+++ Models/EFBooksRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment5_DevynSmith_Section3.Models
+{
+    public class EFBooksRepository : IBooksRepository
+    {
+        private BooksDbContext _context;
+
+        //Constructor
+        public EFBooksRepository (BooksDbContext context)
+        {
+            _context = context;
+        }
+        public IQueryable<Book> Books => _context.Books;
+    }
+}
